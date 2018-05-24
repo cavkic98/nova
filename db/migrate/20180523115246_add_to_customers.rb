8c3bbfb41146c5ab62a248088919ebe9844c3950
Haris Cavkic <dr.cavkic@gmail.com>
2018-05-24 02:01:39 +0000

    izbrisane nepotrebne migracije

--- db/migrate/20180523115246_add_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :approved, :boolean, :default => false, :null => false
-  end
-end
--- db/migrate/20180523121916_remove_approval.rb
+++
@@ -1,5 +0,0 @@
-class RemoveApproval < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :customers, :approved
-  end
-end
